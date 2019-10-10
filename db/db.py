3f9c6f37df8aea83a2f3970aebd6dffcaf4d7b00
Tran Minh Luan <luan@A.local>
2019-10-10 15:11:09 +0700

    Show chat history

--- db/db.py
+++ db/db.py
@@ -18,15 +18,15 @@ class Singleton(type):
         if cls not in cls._instances:
             cls._instances[cls] = super(
                 Singleton, cls).__call__(*args, **kwargs)
-            print("aa")
             connection = pymongo.MongoClient(
                 "mongodb+srv://3man:Admin123@c0-xeenv.mongodb.net/admin?retryWrites=true&w=majority", ssl=True, ssl_cert_reqs=ssl.CERT_NONE)
-
+            print("Init database connection")
             # creating database
             db = connection.chat
 
             cls.user = db.user
             cls.message = db.message
+            cls.socketsession = db.session
 
         return cls._instances[cls]
 
@@ -71,15 +71,12 @@ class Database(metaclass=Singleton):
     def check_valid_cred(self, username, passwd):
         if validate_string(username) == False or validate_string(passwd) == False:
             return "Invalid username or password"
-
-        try:
-            user = self.user.find_one({
-                "username": username,
-            })
-        except pymongo.errors.AutoReconnect:
-            print("a")
+        user = self.user.find_one({
+            "username": username,
+        })
 
         if user == None:
+            print("isdat")
             return "Error has occurred"
 
         if check_password(passwd.encode('utf-8'), user['passwd']):
@@ -94,13 +91,17 @@ class Database(metaclass=Singleton):
         if validate_string(sender) == False or validate_string(receiver) == False:
             return "Error has occurred"
 
-        party = {
+        party = [{
             "sender": sender,
             "receiver": receiver,
-        }
+        },
+            {
+            "sender": receiver,
+            "receiver": sender,
+        }]
 
         try:
-            conversation = self.message.find(party, {"_id": 0})
+            conversation = self.message.find({"$or": party}, {"_id": 0})
         except:
             print("a")
 
@@ -120,6 +121,32 @@ class Database(metaclass=Singleton):
             return "Error has occurred"
 
         return "Success"
+
+    def update_websocket_id(self, username, websocket_id):
+
+        if self.socketsession.count_documents({'username': username}, limit=1) != 0:
+            # update websocket id if already exists
+            result = self.socketsession.update_one(
+                {"username": username}, {"$set": {"websocket_id": websocket_id}})
+        else:
+            result = self.socketsession.insert_one(
+                {"username": username, "websocket_id": websocket_id})
+
+            if result.inserted_id == None:
+                return "Error has occurred"
+
+        return "Success"
+
+    def get_websocket_id_by_username(self, username):
+
+        result = self.socketsession.find_one({
+            "username": username,
+        })
+
+        if result == None or result.get("websocket_id") == None:
+            return 0
+
+        return result["websocket_id"]
 
 
 def get_hashed_password(plain_text_password):
--- http2.py
+++ http2.py
@@ -1,5 +1,5 @@
 from quart import (Quart, abort, jsonify, make_push_promise, render_template,
-                   request, url_for, websocket, Response)
+                   request, url_for, websocket, Response, session)
 from quart_cors import cors, route_cors, websocket_cors
 
 from db import db
@@ -7,6 +7,8 @@ import json
 
 from functools import wraps
 import uuid
+
+import asyncio
 
 
 app = Quart(__name__)
@@ -29,7 +31,6 @@ def collect_websocket(func):
     async def wrapper(*args, **kwargs):
         global connected
         global users
-        print(websocket.keys())
         connected.add(websocket._get_current_object())
         users[id(websocket._get_current_object())
               ] = websocket._get_current_object()
@@ -43,13 +44,66 @@ def collect_websocket(func):
 
 
 @app.websocket('/ws')
-@collect_websocket
+# @collect_websocket
 @websocket_cors(allow_origin='*')
 async def ws():
-    while True:
-        print(users)
-        data = await websocket.receive()
-        await websocket.send(f"echo {data}")
+    global users
+    g_user = ""
+    try:
+        while True:
+            data = await websocket.receive()
+            print("********")
+            # print(type(data))
+            # print(json.dumps(data))
+
+            try:
+                message_json = json.loads(data)
+            except Exception as e:
+                print(e)
+                print("aa")
+
+            if message_json.get("type") == None:
+                await websocket.send("Invalid")
+            else:
+                if message_json["type"] == "init":
+                    g_user = message_json["username"]
+                    print(g_user)
+                    users[g_user] = websocket._get_current_object()
+
+                if message_json["type"] == "chat":
+                    receiver_websocket = users[message_json['receiver']]
+                    await receiver_websocket.send(message_json['content'])
+                    database = db.Database()
+
+                    database.add_message(
+                        message_json['sender'], message_json['receiver'], message_json['content'])
+            database = db.Database()
+            database.update_websocket_id(
+                data, id(websocket._get_current_object()))
+
+            print(users)
+    except asyncio.CancelledError:
+        del users[g_user]
+
+
+# @app.websocket('/submit')
+# @websocket_cors(allow_origin='*')
+# async def sending():
+#     global users
+#     while True:
+#         data = await websocket.receive()
+#         message_json = json.loads(data)
+
+#         print(message_json)
+#         receiver_websocket = users[message_json['receiver']]
+#         receiver_websocket.send(message_json['content'])
+
+#         database = db.Database()
+
+#         database.add_message(
+#             message_json['sender'], message_json['receiver'], message_json['content'])
+
+#         # await websocket.send(f"echo {data}")
 
 
 @app.route('/signup', methods=['POST'])
@@ -69,7 +123,7 @@ async def signup():
         return Response(json.dumps({"error": result}), 404)
 
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['POST', 'GET'])
 @route_cors(allow_origin="*")
 async def login():
     data = await request.get_json()
@@ -93,7 +147,12 @@ async def login():
 @route_cors(allow_origin="*")
 async def list_user():
     data = await request.get_json()
-    print(request.headers['Host'])
+    # print(request.headers['Auth'])
+
+    cred = request.headers['Authorization']
+
+    if cred == None:
+        return Response(json.dumps({"error": "Forbidden"}), 403)
 
     database = db.Database()
 
@@ -106,8 +165,13 @@ async def list_user():
 @route_cors(allow_origin="*")
 async def get_conversation():
     data = await request.get_json()
-    print(request.headers['Authorization'])
-    # party = data['party']
+    receiver = request.args.get('username')
+
+    cred = request.headers['Authorization']
+
+    if cred == None or len(cred.split("&")) != 2:
+        print("a")
+        return Response(json.dumps({"error": "Forbidden"}), 403)
 
     try:
         database = db.Database()
@@ -115,7 +179,15 @@ async def get_conversation():
         print("***********************")
         print(e)
 
-    result = database.show_conversation("luan", "l1uan")
+    username = cred.split("&")[0]
+    password = cred.split("&")[1]
+    isValidUser = database.check_valid_cred(username, password)
+
+    if isValidUser != "Success":
+        print(isValidUser)
+        return Response(json.dumps({"error": "Forbidden"}), 403)
+
+    result = database.show_conversation(username, receiver)
 
     return json.dumps({"data": result})
 
